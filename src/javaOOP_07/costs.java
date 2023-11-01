cf8c6271dc9edf2c2c292fd2500d4f678e4ac00d
Hafizur Rahman Omar <hafizurrahmanomar@gmail.com>
2023-11-01 16:54:16 +0600

    coats update

--- src/javaOOP_07/costs.java
+++ src/javaOOP_07/costs.java
@@ -1,24 +1,22 @@
 package javaOOP_07;
 
-
 public class costs {
      int bazar = 480;
      int cloth = 1400;
      int totalbazarCost = 0;
-     
-public void totalcosts(){
 
-totalbazarCost = bazar+cloth;
-System.out.println("Total bazar costs is : "+totalbazarCost);
-}
+     public void totalcosts() {
 
-//non void mathod
-public int mybazarcost(){
+          totalbazarCost = bazar + cloth;
+          System.out.println("Total bazar costs is : " + totalbazarCost);
+     }
 
-totalbazarCost = bazar+cloth;
-System.out.println("Total bazar costs is : "+totalbazarCost);
-return totalbazarCost;
-}
-    
-    
+     // non void mathod
+     public int mybazarcost() {
+
+          totalbazarCost = bazar + cloth;
+          System.out.println("Total bazar costs is : " + totalbazarCost);
+          return totalbazarCost;
+     }
+
 }
